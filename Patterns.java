7a375b722dc510f6b456290394d93126c4513122
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-06-25 14:41:13 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -21,15 +21,64 @@ public class Patterns {
 // * * * *
 // * * * * *
 
-        for(int i =0;i<5;i++)
-        {
-            for(int j=0;j<=i;j++)
-            {
-                System.out.print('*');
+//         for(int i =0;i<5;i++)
+//         {
+//             for(int j=0;j<=i;j++)
+//             {
+//                 System.out.print('*');
 
-            }
-            System.out.println();
-        }
+//             }
+//             System.out.println();
+//         }
+
+// pattern
+// 1
+// 12
+// 123
+// 1234
+// 12345
+    
+//         for(int i =1;i<=5;i++)
+//         {
+//             for(int j=1;j<=i;j++)
+//             {
+//                 System.out.print(j);
+
+//             }
+//             System.out.println();
+//         }
+// for(int i=1;i<=5;i++)
+// {
+//     for(int j=0;j<i;j++)
+//     {
+//         System.out.print(i);
+//     }
+//     System.out.println();
+// }
+
+// pattern
+// *****
+// ****
+// ***
+// **
+// for(int i=0;i<5;i++)
+// {
+//     for(int j=5;j>i;j--)
+//     {
+//         System.out.print('*');
+
+//     }
+//     System.out.println();
+// }
+
+for(int i=5;i<0;i--)
+{
+    for(int j=1;j<=i;j++)
+    {
+        System.out.println(j);
+    }
+    System.out.println();
+}
 
     }
     
