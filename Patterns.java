8dd7f17182edf0bb240912429d29ae1f7497a72e
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-06-27 13:54:03 +0530

    pattern

--- Patterns.java
+++ Patterns.java
@@ -70,15 +70,73 @@ public class Patterns {
 //     }
 //     System.out.println();
 // }
+// pattern 
+// 1 2 3 4 5 6
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2 
+// 1
 
-for(int i=5;i<0;i--)
-{
-    for(int j=1;j<=i;j++)
-    {
-        System.out.println(j);
-    }
-    System.out.println();
-}
+// for(int i=5;i<0;i--)
+// {
+//     for(int j=1;j<=i;j++)
+//     {
+//         System.out.println(j);
+//     }
+//     System.out.println();
+// }
+// pattern
+// *    
+// ***
+// *****
+// *******
+// *********
+
+// for(int i=0;i<5;i++)
+// {
+//     for(int j=0;j<5-i-1;j++)
+//     {
+//         System.out.print(" ");
+
+//     }
+//     for(int j=0;j<2*i+1;j++)
+//     {
+//         System.out.print("*");
+//     }
+//     for(int j=0;j<5-i-1;j++)
+//     {
+//         System.out.print(" ");
+
+//     }
+//     System.out.println();
+
+
+// }
+// pattern 
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// for(int i=0;i<5;i++)
+// {
+//     for(int j=0;j<i;j++)
+//     {
+//         System.out.print(" ");
+//     }
+//     for(int j=0;j<2*5-(2*i+1);j++)
+//     {
+//         System.out.print("*");
+//     }
+//     for(int j=0;j<i;j++)
+//     {
+//         System.out.print(" ");
+//     }
+//     System.out.println();
+
+// }
+
 
     }
     
---
+++ eraserPensil.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class eraserPensil {
+    public static void main(String[] args) {
+        int totalPencil,totalPen,cPencil,cPen;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter total pencil");
+        totalPencil=sc.nextInt();
+        System.out.println("enter total pen");
+        totalPen=sc.nextInt();
+        System.out.println("enter total pencil");
+        totalPencil=sc.nextInt();
+        System.out.println("enter total pencil");
+        totalPencil=sc.nextInt();
+
+
+    }
+    
+}
