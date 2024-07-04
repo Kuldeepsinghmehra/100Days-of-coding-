4848596d236f285b3e93135cd3f6995dc30fd099
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-07-04 09:33:09 +0530

    Create leftRotate.java

---
+++ leftRotate.java
@@ -0,0 +1,22 @@
+public class leftRotate {
+    static void solve(int arr[], int n) {
+        int first=arr[0];
+        for(int i=0;i<n-1;i++)
+        {
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=first;
+        for (int i : arr) {
+            System.out.print(i+" ");
+            
+        }
+        }
+    public static void main(String[] args) {
+        int n=5;
+  
+        int arr[]= {1,2,3,4,5};
+        solve(arr, n);
+        
+    }
+    
+}
