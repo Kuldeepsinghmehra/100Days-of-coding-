8e6b06fddaf3c8ae416599537a18015bca92ddc0
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-07-23 12:04:28 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static boolean CheckAnagram(String s1,String s2)
+    {
+       char[] s1Check= s1.toCharArray();
+       char[] s2Check= s2.toCharArray();
+       Arrays.sort(s1Check);
+       Arrays.sort(s2Check);
+       return Arrays.equals(s1Check, s2Check);
+    }
+    public static void main(String[] args) {
+        String s1,s2;
+        s1="listen";
+        s2="Ailent";
+       boolean Result= Anagram.CheckAnagram(s1, s2);
+        System.out.println("Anagram result is "+Result);
+    }
+    
+}
