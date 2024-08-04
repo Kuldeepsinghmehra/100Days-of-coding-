edb8b9cb46ab0d31629bb44d749935821f557c23
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-08-04 14:22:16 +0530

    Update outerParenthesis.java

--- outerParenthesis.java
+++ outerParenthesis.java
@@ -1,15 +1,53 @@
 public class outerParenthesis {
-    public static void ReturnAnswer(String p)
+    public static void ReturnAnswerBrute(String p)
     {
-        int start=0;
-        
+        int count=0;
+        String res="";
+
         for (int i=0;i<p.length();i++)
         {
-
+            if(p.charAt(i)=='(' && count++>0)
+            {
+                res+=p.charAt(i);
+                
+            }
+            if(p.charAt(i)==')' && count-->1)
+            {
+                res+=p.charAt(i);
+             
+            }
         }
+        System.out.println(res);
+    }
+    public static void ReturnAnswerOptimal(String p)
+    {
+        int opening=0;
+        int closing=0;
+        int start=0;
+        String res="";
+        for(int i=0;i<p.length();i++)
+        {
+            if(p.charAt(i)=='(')
+            {
+                opening++;
+            }
+            else if (p.charAt(i)==')') {
+                closing++;
+                
+            }
+            if (opening==closing) {
+                res+=p.substring(start+1, i);
+                start=i;
+            }
+            
+        }
+        System.out.println(res);
+
     }
     public static void main(String[] args) {
-        String S = "(()())(())()";
+        String S = "((()))(())";
+        outerParenthesis.ReturnAnswerBrute(S);
+        outerParenthesis.ReturnAnswerBrute(S);
     }
     
 }
