a554df6da5127daa9afc47f43f4fa4c600cba9f2
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-08-07 20:33:30 +0530

    Create kaprekarNQT.java

---
+++ kaprekarNQT.java
@@ -0,0 +1,25 @@
+public class kaprekarNQT {
+   public static int isKaprekar(int N) {
+        // code here
+        int square= N*N;
+        String result=Integer.toString(square);
+        int size=result.length();
+       for(int i=1;i<size;i++)
+       {
+           String left=result.substring(0,i);
+           String right=result.substring(i);
+           int leftnum=left.isEmpty() ? 0 : Integer.parseInt(left);
+           int rightnum=right.isEmpty() ? 0: Integer.parseInt(right);
+           if(leftnum!=0 && rightnum!=0 && leftnum+rightnum==N){
+               return 1 ;
+           }
+       }
+       return 0;
+    }
+    public static void main(String[] args) {
+        int num=45;
+        kaprekarNQT.isKaprekar(num);
+        
+    }
+    
+}
