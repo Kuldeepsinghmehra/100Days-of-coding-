342f8bedcd6fc6e0fb003fb3312854647babce95
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-08-21 23:20:47 +0530

    Update leftrotateDplaces.java

--- leftrotateDplaces.java
+++ leftrotateDplaces.java
@@ -1,5 +1,22 @@
 public class leftrotateDplaces {
-    public static void returnArray(int[] arr,int N,int D)
+    public static void LeftreturnArray(int[] arr,int N,int D)
+    {
+        int[] temp=new int[N];
+        for(int i=0;i<D;i++)
+        {
+            temp[i]=arr[i];
+        }
+        for(int i=0;i<N-D;i++)
+        {
+            arr[i]=arr[i+D];
+        }
+        for(int i=N-D;i<N;i++)
+        {
+            arr[i]=temp[i-N+D];
+        }
+          
+    }
+    public static void RightreturnArray(int[] arr,int N,int D)
     {
         int[] temp=new int[N];
         for(int i=N-D;i<N;i++)
@@ -15,7 +32,6 @@ public class leftrotateDplaces {
         {
             arr[i]=temp[i];
         }
-       
 
     }
  
@@ -23,12 +39,21 @@ public class leftrotateDplaces {
         int[] arr={1,2,3,4,5,6,7};
         int k=2;
         int length=arr.length;
-        leftrotateDplaces.returnArray(arr, length,k);
+        leftrotateDplaces.RightreturnArray(arr, length,k);
         System.out.println("after rotating right");
         for(int i=0;i<arr.length;i++)
         {
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
+     
+        System.out.println("After left rotating");
+        leftrotateDplaces.LeftreturnArray(arr, length, k);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        
         
     }
     
